f323115ade8e245a663911a737db7bd5d62ff240
Luis Jimenez <luisrrjjcc@gmail.com>
2022-09-04 20:40:49 -0600

    inicial

---
+++ assets/info.js
@@ -0,0 +1,10 @@
+var info = [
+        {link:"abc", name:"Luis", companion:"Prueba", c:0},
+        {link:"def", name:"Ricardo", c:1}
+];
+
+var conf = [
+        "Luis\n32500836",
+        "Yosi\n30285420",
+        "Martha\n59004842"
+]
