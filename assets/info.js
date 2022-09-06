d750dd971068c61ffe2df026ae6b4c85240a23f8
Luis Jimenez <luisrrjjcc@gmail.com>
2022-09-05 22:57:53 -0600

    links

--- assets/info.js
+++ assets/info.js
@@ -1,6 +1,53 @@
 var info = [
-        {link:"test1", name:"Alice Ejemplo", companion:"Bob Ejemplo", c:0},
-        {link:"test2", name:"Carl Ejemplo", c:1}
+        {link:"lourdesruben", name:"Lourdes Guevara y Rubén Téllez", c:2, sp:"dos personas."},
+        {link:"raquelgustavo", name:"Raquel Orellana y Gustavo Sosa", c:2, sp:"dos personas."},
+        {link:"maribelwilmer", name:"Maribel Morales y Wilmer Valenzuela", c:2, sp:"dos personas."},
+        {link:"marialuis", name:"María De León y Luis De León", c:2, sp:"dos personas."},
+        {link:"odiliaruano", name:"Odilia Ruano", c:2},
+        {link:"luisarevalo", name:"Luis Arévalo", c:2},
+
+        {link:"mariayat", name:"María Yat", c:2},
+        {link:"dalilafamilia", name:"Dalila López,", companion:"Gerbi Pacheco, José Pachecho y Jaqueline Pacheco", c:2, sp:"cuatro personas."},
+
+        {link:"marthacu", name:"Martha Cu", c:0},
+        {link:"ricardopetrona", name:"Ricardo Cu y Petrona Yat", c:0, sp:"dos personas."},
+        {link:"luislucreciasebastian", name:"Luis Cu\nLucrecia De León\ny Sebastián Cu", c:0, sp:"tres personas."},
+        {link:"mariocu", name:"Mario Cu", c:0},
+
+        {link:"ricardoanalu", name:"Ricardo Goubaud", companion:"Ana Lucía Asencio", c:0},
+        {link:"christianmerlin", name:"Christian Brolo", companion:"Merlin Valle", c:0},
+        {link:"joaquinmaoly", name:"Joaquín Saquilmer", companion:"Maoly Morales", c:0},
+        {link:"adolfomonica", name:"Adolfo Acevedo", companion:"Mónica Flores", c:0},
+        {link:"edwineloiza", name:"Edwin Cerna", companion:"Eloiza Garrido", c:0},
+        {link:"alejandrosiddy", name:"Alejandro Pereda y Siddy Bustamante", c:0, sp:"dos personas."},
+        {link:"andrespinzon", name:"Andrés Pinzón", c:0},
+        {link:"juliomelendrez", name:"Julio Melendrez", c:0},
+        {link:"evelyncruz", name:"Evelyn Cruz", c:0},
+        {link:"catherinechen", name:"Catherine Chen", c:0},
+        {link:"tiffanybustamante", name:"Tiffany Bustamante", c:0},
+        {link:"gerardohuerta", name:"Gerardo Huerta", c:0},
+        {link:"herbertlopez", name:"Herbert Lopez", c:0},
+
+        {link:"ivonemorales", name:"Ivone Morales", c:1},
+        {link:"bryanyaneth", name:"Bryan Espinoza y Yaneth Oseguera", c:1, sp:"dos personas."},
+        {link:"abigaildavid", name:"Abigail Espinoza y David Teque", c:1, sp:"dos personas."},
+        {link:"darlinespinoza", name:"Darlin Espinoza", companion:"acompañante", c:1},
+        {link:"leticiavalerio", name:"Leticia Cifuentes y Valerio Morales", c:1, sp:"dos personas."},
+        {link:"fabriciocalebnadia", name:"Fabricio Ganuza, Caleb Ganuza y Nadia Ganuza", c:1, sp:"tres personas."},
+        {link:"dulcedaniela", name:"Dulce Morales y Daniela Melchor", c:1, "dos personas."},
+        {link:"evamorales", name:"Eva Morales", c:1},
+        {link:"gabrielaluis", name:"Gabriela Arriola y Luis Pedro Rosales", c:1, sp:"dos personas."},
+        {link:"rafaelhuertas", name:"Rafael Huertas", c:1},
+        {link:"marianaaranda", name:"Mariana Aranda", c:1},
+        {link:"luislopez", name:"Luis Ángel López", c:1},
+        {link:"liliantique", name:"Lilian Tique", c:1},
+        {link:"esthersalazar", name:"Esther Salazar", c:1},
+        {link:"julioflores", name:"Julio Flores", companion:"acompañante", c:1},
+        {link:"koenraadsaguil", name:"Koenraad Saguil", c:1},
+        {link:"arielsoto", name:"Ariel Soto", c:1},
+        {link:"bileyviricardo", name:"Bileyvi Morales y Ricardo Morales", c:1, sp:"dos personas."},
+        {link:"alexmorales", name:"Alex Morales", companion:"acompañante", c:1},
+        {link:"isabelhidalgo", name:"Isabel Hidalgo", companion:"acompañante", c:1}
 ];
 
 var conf = [
